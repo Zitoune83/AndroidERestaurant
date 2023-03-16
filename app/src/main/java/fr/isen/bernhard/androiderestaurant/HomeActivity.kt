d21decd93a645d0b40d446638479774e5cf8ceff
Olivier Bernhard <olive83190@outlook.fr>
2023-03-16 21:31:44 +0100

    Add Log on HomeActivity when onDestroy()

--- app/src/main/java/fr/isen/bernhard/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/bernhard/androiderestaurant/HomeActivity.kt
@@ -2,10 +2,19 @@ package fr.isen.bernhard.androiderestaurant
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class HomeActivity : AppCompatActivity() {
+
+    private val TAG = "HomeActivity"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+    }
+
+     override fun onDestroy() {
+         super.onDestroy()
+         Log.d(TAG,"HomeActivity destroyed")
     }
 }
