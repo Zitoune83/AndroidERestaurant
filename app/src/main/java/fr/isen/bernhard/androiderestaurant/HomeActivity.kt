6cc3c4a3f179e15a60af9f8d966c9637886d761f
Olivier Bernhard <olive83190@outlook.fr>
2023-03-16 22:14:14 +0100

    Feature binding on HomeActivity

--- app/src/main/java/fr/isen/bernhard/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/bernhard/androiderestaurant/HomeActivity.kt
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import fr.isen.bernhard.androiderestaurant.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private val tag = "HomeActivity"
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         //Recupere l' @ de l' objet homeButton1
         //val button: Button = findViewById(R.id.homeButton1)
-
+        //binding.homeButton1.setOnClickListener {
+         //   println("ROOOOOOOOOOOL")
+       // }
 
 
     }
