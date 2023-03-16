454f6618acba745d80c83640a72d98e5516edd86
Olivier Bernhard <olive83190@outlook.fr>
2023-03-16 22:00:41 +0100

    Add 3 buttons on activity_home

--- app/src/main/java/fr/isen/bernhard/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/bernhard/androiderestaurant/HomeActivity.kt
@@ -3,18 +3,25 @@ package fr.isen.bernhard.androiderestaurant
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class HomeActivity : AppCompatActivity() {
 
-    private val TAG = "HomeActivity"
+    private val tag = "HomeActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //Recupere l' @ de l' objet homeButton1
+        //val button: Button = findViewById(R.id.homeButton1)
+
+
+
     }
 
      override fun onDestroy() {
          super.onDestroy()
-         Log.d(TAG,"HomeActivity destroyed")
+         Log.d(tag,"HomeActivity destroyed")
     }
 }
